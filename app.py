cac62eaf72b8576578c546030c953d38552ad2d5
amannaik247 <amancodes247@gmail.com>
2024-09-24 01:47:50 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ llm = ChatGroq(groq_api_key=groq_api_key, model_name="llama-3.1-70b-versatile")
 
 # Initialize MongoDB client
 
-client = MongoClient(mongo_uri)
+client = MongoClient(mongo_uri, ssl_cert_reqs=False)
 db = client["krishnaji_chatbot"]  # Database name
 collection = db["bhagavad_gita"]  # Collection name
 
